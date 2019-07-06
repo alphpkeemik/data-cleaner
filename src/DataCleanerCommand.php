48db3a4bcf066c7df06d3fcc530426d2d6f0c845
Mati Andreas <mati.andreas@ambientia.ee>
2019-07-06 15:59:54 +0300

    AMBESTINT-77 add command

---
+++ src/DataCleanerCommand.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the Ambientia DataCleaner package.
+ *
+ * (c) Ambientia Estonia OÜ
+ */
+
+namespace Ambientia\DataCleaner;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @author mati.andreas@ambientia.ee
+ */
+class DataCleanerCommand extends Command
+{
+    /**
+     * @var DataCleaner
+     */
+    private $dataCleaner;
+
+    /**
+     * @param DataCleaner $dataCleaner
+     *
+     * @required
+     */
+    public function setDataCleaner($dataCleaner)
+    {
+        $this->dataCleaner = $dataCleaner;
+    }
+
+    protected function configure()
+    {
+        parent::configure();
+        $this->setName('ambientia:data-cleaner');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->dataCleaner->execute();
+    }
+}
---
+++ tests/CommandTest.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the Ambientia DataCleaner package.
+ *
+ * (c) Ambientia Estonia OÜ
+ */
+
+namespace Ambientia\DataCleaner\Tests;
+
+
+use Ambientia\DataCleaner\DataCleaner;
+use Ambientia\DataCleaner\DataCleanerCommand;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CommandTest extends TestCase
+{
+
+
+    public function testTest()
+    {
+        $dataCleaner = $this->createMock(DataCleaner::class);
+
+        $dataCleaner
+            ->expects($this->once())
+            ->method('execute');
+
+        $command = new DataCleanerCommand();
+        $command->setDataCleaner($dataCleaner);
+        $inputInterface = $this->createMock(InputInterface::class);
+        $outputInterface = $this->createMock(OutputInterface::class);
+        $command->run($inputInterface, $outputInterface);
+
+    }
+}
